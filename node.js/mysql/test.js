83034d36d752965021c9883e2d3b18a4881713fa
cloudyar <635132559@qq.com>
2018-07-01 00:49:22 +0800

    modified the test.js

--- node.js/mysql/test.js
+++ node.js/mysql/test.js
@@ -1,14 +1,24 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+
+var dbconfig = {
 	host: 'localhost',
 	user: 'root',
 	password: 'root',
 	database: 'test'
+};
+//创建连接
+var connection = mysql.createConnection(dbconfig);
+//连接数据库connection.connect();
+connection.connect(function(err) {
+	if(err) {
+		console.log('连接错误： ' + err.stack);
+		return;
+	}
+	console.log('连接ID： ' + connection.threadId);
 });
-connection.connect();
 var sql = 'select * from websites';
 //查询
-connection.query(sql, function(err, result) {
+connection.query(sql, function(err, result, fields) {
 	if(err) {
 		console.log('[SELECT ERROR] - ', err.message);
 		return;
@@ -16,5 +26,18 @@ connection.query(sql, function(err, result) {
 	console.log('--------------select-------------');
 	console.log(result);
 	console.log('---------------------------------');
+});
+var addSql = 'insert into websites(Id,name,url,alexa,country) values(0,?,?,?,?)';
+var addSqlParams = ['菜鸟工具', 'https://c.runnoob.com', '23453', 'CN'];
+//增加一条记录
+connection.query(addSql, addSqlParams, function(err, result) {
+	if(err) {
+		console.log('[INSERT ERROR] - ',err.message);
+		return;
+	}
+	console.log('--------------------------INSERT----------------------------');
+	//console.log('INSERT ID:',result.insertId); 
+	console.log('INSERT ID:',result);
+	console.log('-----------------------------------------------------------------\n\n');
 });
 connection.end();
